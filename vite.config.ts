5d9a30b7022cc3dad07b8172641f2c282858b079
pyabhi114 <abhishekgogna36@gmail.com>
2025-11-16 05:11:59 +0530

    updaetd config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/little-learn-lights-main/",
   server: {
     host: "::",
     port: 8080,
